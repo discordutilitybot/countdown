8dbf297de4a6cf388fa5c8b11cd3a1813e0ceafd
PineappleRind <pineapplerind.info@gmail.com>
2021-01-06 10:53:19 -0500

    index.js lil update

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 var countDownDate = new Date("Feb 1, 2021 12:00:00").getTime();
-function timer() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-        
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-    document.getElementById("timer").innerHTML = days + "d  " + hours + "h  "
-    + minutes + "m  " + seconds + "s";
-}
 
+function timer() {
+    var e = (new Date).getTime(),
+        t = countDownDate - e,
+        n = Math.floor(t / 864e5),
+        o = Math.floor(t % 864e5 / 36e5),
+        r = Math.floor(t % 36e5 / 6e4),
+        a = Math.floor(t % 6e4 / 1e3);
+    document.getElementById("timer").innerHTML = n + "d  " + o + "h  " + r + "m  " + a + "s"
+}
 var x = setInterval(function() {
-    timer();
-}, 1000);
+    timer()
+}, 1e3);
 timer();
